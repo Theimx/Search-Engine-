03b1360df06dcd465d8e1bf506308383dba43d59
Theimx <85684007+Theimx@users.noreply.github.com>
2023-12-15 22:51:36 +0100

    Add files via upload

---
+++ ConstructorIndex.py
@@ -0,0 +1,14 @@
+#A file for creating an index in a python file with a text file and an Hashing function : 
+#Word ==>
+#value = Hash(Word) ==>
+#value = Word 
+from Hashing_function import hash
+
+#piece of code to return the number of line of the dictionnary 
+with open(r"FrenchDictionary.txt", 'r') as fp:
+    for count, line in enumerate(fp):
+        pass
+print('Total Number of lines:', count + 1)
+
+
+print(hash("hello"))
