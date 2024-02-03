60a8153895686766f341a186da683a6639ef0183
Theimx <85684007+Theimx@users.noreply.github.com>
2024-02-03 13:52:09 +0100

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,21 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-URL = "https://info.blaisepascal.fr/gerer-une-base-de-donnees-avec-python/"
+URL = "https://movies2watch.to/watch-movie/watch-spider-man-into-the-spider-verse-sequel-full-66674.9653188"
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
-titles0 = soup.find_all("title")
-for title0 in titles0:
-  print(title0)
+elementsToFind = ["title","h1","h2"]
+link = "a"
+urlOnThePage = []
+result = []
 
-titles1 = soup.find_all("h1")
-for title1 in titles1:
-  print(title1)
+x = soup.find_all(elementsToFind[0])
+for y in x:
+  result.append(y)
 
-titles2 = soup.find_all("h2")
-for title2 in titles2:
-  print(title2)
-
-hp = soup.find_all("a")
-for hp1 in hp:
-  print(hp1)
