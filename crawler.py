d84e1e8564b5c3ce658441567f9832cb429dafab
Theimx <85684007+Theimx@users.noreply.github.com>
2024-02-05 21:56:04 +0100

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,7 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-Web=requests.get("https://www.google.com/")
-soup=BeautifulSoup(Web.text,'lxml')
-for link in soup.findAll('a'):
-    print(link['href'])
+def extraire_liens(url):
+
+    liste_liens = []
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        liens = soup.find_all('a')
+
+        for lien in liens:
+            href = lien.get('href')
+            if href and href.startswith('http'):
+                liste_liens.append(href)
+
+        return liste_liens
+    else:
+        print(f"Échec de la requête. Code d'état : {response.status_code}")
+        return None
+
+#-----------------------------------------------------
+url = 'https://numpy.org/'
+liens_trouves = extraire_liens(url)
+
+
+for lien in liens_trouves:
+    print(lien)
