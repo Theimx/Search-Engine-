b0a8b6e836c6f82065e61846011e34af8528394b
Theimx <85684007+Theimx@users.noreply.github.com>
2024-02-06 16:27:56 +0100

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,14 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-def extraire_liens(url):
+def extraire_informations(url):
 
     liste_liens = []
+    liste_titres = []
+    liste_sous_titres = []
+
     response = requests.get(url)
 
     if response.status_code == 200:
-        
+
         soup = BeautifulSoup(response.text, 'html.parser')
+
         liens = soup.find_all('a')
 
         for lien in liens:
@@ -16,15 +20,28 @@ def extraire_liens(url):
             if href and href.startswith('http'):
                 liste_liens.append(href)
 
-        return liste_liens
+        titres = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+
+        for titre in titres:
+            liste_titres.append(titre.text.strip())
+
+        sous_titres = soup.find_all(['h2', 'h3', 'h4', 'h5', 'h6'])
+
+        for sous_titre in sous_titres:
+            liste_sous_titres.append(sous_titre.text.strip())
+
+        return liste_liens, liste_titres, liste_sous_titres
     else:
         print(f"Échec de la requête. Code d'état : {response.status_code}")
-        return None
+        return None, None, None
 
-#-----------------------------------------------------
+#---------------------------------------------------------------------------------------------
 url = 'https://numpy.org/'
-liens_trouves = extraire_liens(url)
+liens, titres, sous_titres = extraire_informations(url)
 
-
-for lien in liens_trouves:
+for lien in liens:
     print(lien)
+for titre in titres:
+    print(titre)
+for sous_titre in sous_titres:
+    print(sous_titre)
